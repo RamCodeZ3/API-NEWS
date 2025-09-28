c019edd3ecfa4b10c80c69f9839f5de00e67a2d6
RamCodeZ3 <arammusset7@gmail.com>
2025-09-28 15:15:16 -0400

    Se creo un nuevo modulo en news_pages donde se van extraer todos los articulos de manera individuales de las difetentes paginas, el nuevo modulo fue para free diary

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from news.free_diary import FreeDiary
 
-
 if __name__ == '__main__':
     scrapper = FreeDiary()
     print(scrapper.news_free_diary())
---
+++ src/news/news_pages/page_free_diary.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+
+class PageFreeDiary:
+    def __init__(self):
+        self.body_text = ""
+
+    def page_free_diary(self, url):
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+
+        time.sleep(5)
+
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+
+        article = soup.find("div", class_="detail-body")
+        paragraphs = article.find_all("p")
+
+        for p in paragraphs:
+            self.body_text += p.get_text(strip=True)
+        
+        driver.quit()
+        return self.body_text
+
